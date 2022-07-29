c593f3ed577324fab3b4b219da5a2b9d97a4df2a
Vogrant <vograntt@gmail.com>
2022-07-29 14:16:16 +0300

    hw3 done

--- HW3/main.js
+++ HW3/main.js
@@ -240,7 +240,14 @@
 // //         default:
 // //             alert( 'We hope that this page looks ok!' );
 // //     }
-// //
+// let browser = prompt('Chooce your browser');
+// if ( browser === 'Edge'){
+//     alert("You've got the Edge!")
+// }  else if ( browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' ||browser === 'Opera'){
+//     alert( 'Okay we support these browsers too' );
+// } else{
+//     alert( 'We hope that this page looks ok!' );
+// }
 // //     14 - Задача (свитч кейс)
 // //     Переписать условия "if" на "switch"
 // //     важность: 4
@@ -259,7 +266,19 @@
 // //         if (number === 2 || number === 3) {
 // //             alert('Вы ввели число 2, а может и 3');
 // //         }
-// //
+const number = +prompt('Введите число между 0 и 3', '');
+switch (number) {
+    case 0:
+        alert('Вы ввели число 0');
+        break;
+    case 1:
+        alert('Вы ввели число 1');
+        break;
+    case 2:
+    case 3:
+        alert('Вы ввели число 2, а может и 3');
+        break;
+}
 // //
 // //
 // //
